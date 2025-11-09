f33617979d119204a1d1e3a2be0bfb71fd68acbd
katyrose28 <146095444+katyrose28@users.noreply.github.com>
2025-11-08 19:28:27 -0500

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,145 @@
+delts = [
+    ("Lateral Raise", 20),
+    ("Arnold Press", 30),
+    ("Military Press", 30),
+    ("Rear Delt Row", 25),
+    ("Face Pull", 20),
+    ("Hip Huggers", 25),
+    ("Front Raise", 20),
+    ("Shoulder Press", 45),
+]
+
+chest = [
+    ("Pushups", 0),
+    ("Floor Fly", 25,),
+    ("Pullovers", 35),
+    ("Cross Overs", 20),
+    ("Bench Press", 100),
+    ("Incline Bench Press", 70),
+    ("Center Press", 40)
+]
+
+biceps = [
+    ("Zottman Curls", 20),
+    ("Preacher Curls", 40),
+    ("Drag Curls", 40),
+    ("Waiter Curls", 30),
+    ("Incline Curls", 20),
+    ("DB Curls", 20),
+    ("Reverse Curls", 15),
+    ("Hammer Curls", 20)
+]
+
+butt = [
+    ("Goblet Squat", 45),
+    ("Sumo Squat", 50),
+    ("Step-ups", 25),
+    ("Deadlift", 115),
+    ("Squat", 115)
+]
+
+back_lats = [
+    ("Single Arm Row", 25),
+    ("Dumbell Pullover", 35),
+    ("Seal Row", 30),
+    ("Incline Row", 25),
+    ("Lat Pull Down", 20),
+    ("Shrugs", 125)
+]
+
+back_mids = [
+    ("Around the World", 20),
+    ("Scap Squeeze", 100),
+    ("Landmind Row", 70),
+    ("Supinated Row", 80)
+]
+
+back_lower = [
+    ("Good Mornings", 100),
+    ("Rack Pull", 125),
+    ("Stiff Leg Deadlift", 115),
+    ("Back Extension", 25)
+]
+
+back_combo = [
+    ("DB Lift March", 30), 
+    ("Gorilla Row", 25),
+    ("Renegade Row", 10),
+    ("Dead Row", 80),
+    ("Inverted Row (like a pullup)", 0),
+    ("Farmer Walke", 40)
+]
+
+abs_upper = [
+    ("Around the world", 25),
+    ("Side Bend", 35),
+    ("Standing Twist", 10),
+    ("Figure 8's", 15),
+    ("Standing Crunch", 20),
+    ("Hip Dip", 0),
+    ("Spider Plank", 15)
+]
+
+abs_lower = [
+    ("Side-to-Side", "ankle weights"),
+    ("Up-and-Over", "ankle weights"),
+    ("Cross Taps", 10),
+    ("Reverse Crunches", 0),
+    ("Butterflies", 0),
+    ("Side Crunches", 0),
+    ("Heel Touches", 0)
+
+]
+
+abs_combo = [
+    ("Side Carry", 40),
+    ("Bridge March", 35),
+    ("Spider Pulls", 15)
+]
+
+triceps = [
+    ("Pull Downs", "cables"),
+    ("Reverse Grip Pull Downs", "cables"),
+    ("Kickback", 25),
+    ("Lying Tricep Extension - Pulse", 10),
+    ("Lying Tricep Extension - In and Out", 10),
+    ("Skull Crushers", 40),
+    ("Narrow Grip Bench Press", 60)
+]
+
+calf = [
+    ("Standing", 30),
+    ("Seated", 30),
+    ("Bent Knee", 30),
+    ("Wide", 30),
+    ("Inner (toes pointed in)", 30),
+    ("Single", 30),
+    ("Box Raise", 30)
+]
+
+thighs = [
+    ("Clam Shells", "band"),
+    ("Leg Extensions", "ankle weights"),
+    ("Side Lunge", 15),
+    ("Fire Hydrant/Donkey Kick", "band"),
+    ("Curtsey Lunge", 10),
+    ("Scissor Kick", "ankle weights"),
+    ("Bridges", 100)
+]
+
+all_groups = {
+    "Delts": delts,
+    "Chest": chest,
+    "Biceps": biceps,
+    "Butt": butt,
+    "Back Lats": back_lats,
+    "Back Mids": back_mids,
+    "Back Lower": back_lower,
+    "Back Combo": back_combo,
+    "Abs Upper": abs_upper,
+    "Abs Lower": abs_lower,
+    "Abs Combo": abs_combo,
+    "Triceps": triceps,
+    "Calves": calf,
+    "Thighs": thighs,
+}
